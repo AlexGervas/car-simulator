97452aa765c31787c6fab4b83bf388a784e77d69
AlexGervas <alexandragervas@gmail.com>
2025-03-05 20:23:21 +0700

    Update path and file for deploy for 404.html

---
+++ copy-404.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const source = path.join(__dirname, 'src', '404.html');
+const destination = path.join(__dirname, 'dist', 'car-simulator', 'docs', '404.html');
+
+fs.copyFile(source, destination, (err) => {
+  if (err) {
+    console.error('Error copying file:', err);
+  } else {
+    console.log('404.html was copied to dist/car-simulator/docs/');
+  }
+});
