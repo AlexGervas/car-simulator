7825f1264940f5456c720816baf9f39b08e80b57
AlexGervas <alexandragervas@gmail.com>
2025-03-05 19:14:21 +0700

    Added 404 component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { StartPageComponent } from './modules/start-page/start-page.component';
 import { SimulatorComponent } from './modules/simulator/simulator.component';
+import { NotFoundComponent } from './modules/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', component: StartPageComponent },
-    { path: 'game', component: SimulatorComponent }
+    { path: 'game', component: SimulatorComponent },
+    { path: '**', component: NotFoundComponent }
 ];
