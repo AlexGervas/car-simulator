5921b73107f6f502f07525775d0cdd16f351bbbf
AlexGervas <alexandragervas@gmail.com>
2025-04-25 16:37:19 +0700

    Added a loader for the model view component

--- src/app/modules/pages/model-viewer/model-viewer.component.ts
+++ src/app/modules/pages/model-viewer/model-viewer.component.ts
@@ -3,11 +3,14 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+import { LoaderComponent } from '../../../shared/loader/loader.component';
+import { CommonModule } from '@angular/common';
+import { ModelsLoaderService } from '../../../core/services/models-loader.service';
 
 @Component({
   selector: 'app-model-viewer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, LoaderComponent],
   templateUrl: './model-viewer.component.html',
   styleUrl: './model-viewer.component.css'
 })
@@ -30,116 +33,135 @@ export class ModelViewerComponent implements OnInit {
 
   private animationFrameId!: number;
 
-  private isFrontLeftDoorOpen = false;
-  private isFrontRightDoorOpen = false;
-  private isBackLeftDoorOpen = false;
-  private isBackRightDoorOpen = false;
-  private isCarTrunkOpen = false;
-  private isCarHoodOpen = false;
+  private isFrontLeftDoorOpen: boolean = false;
+  private isFrontRightDoorOpen: boolean = false;
+  private isBackLeftDoorOpen: boolean = false;
+  private isBackRightDoorOpen: boolean = false;
+  private isCarTrunkOpen: boolean = false;
+  private isCarHoodOpen: boolean = false;
 
   private doorGroups: { [key: string]: THREE.Object3D[] } = {};
 
-  constructor(private el: ElementRef) { }
+  constructor(private modelsLoaderService: ModelsLoaderService) { }
 
   ngOnInit(): void {
-    this.initScene();
-    this.loadModel();
+    this.modelsLoaderService.show();
+    this.initScene().then(() => {
+      return this.loadModel();
+    }).then(() => {
+      console.log('Model loaded successfully');
+    }).catch((error) => {
+      console.error('Failed to load resources:', error);
+    });
     this.animate();
   }
 
-  private initScene(): void {
-    this.scene = new THREE.Scene();
-    // this.scene.background = new THREE.Color(0xc0c0c0);
+  private initScene(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.scene = new THREE.Scene();
+      // this.scene.background = new THREE.Color(0xc0c0c0);
 
-    const container = this.viewerContainer.nativeElement;
-    this.camera = new THREE.PerspectiveCamera(70, container.clientWidth / container.clientHeight, 0.1, 1000);
-    this.camera.position.set(2, 3, 5);
-    this.camera.lookAt(0, 0, 0);
+      const container = this.viewerContainer.nativeElement;
+      this.camera = new THREE.PerspectiveCamera(70, container.clientWidth / container.clientHeight, 0.1, 1000);
+      this.camera.position.set(2, 3, 5);
+      this.camera.lookAt(0, 0, 0);
 
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    this.renderer.setSize(container.clientWidth, container.clientHeight);
-    this.renderer.shadowMap.enabled = true;
-    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-    container.appendChild(this.renderer.domElement);
+      this.renderer = new THREE.WebGLRenderer({ antialias: true });
+      this.renderer.setSize(container.clientWidth, container.clientHeight);
+      this.renderer.shadowMap.enabled = true;
+      this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+      container.appendChild(this.renderer.domElement);
 
-    const rgbeLoader = new RGBELoader();
-    // small_hangar_01_4k, rooftop_night_4k
-    rgbeLoader.load('hdri/small_hangar_01_4k.hdr', (texture) => {
-      texture.mapping = THREE.EquirectangularReflectionMapping;
-      this.scene.background = texture;
-      this.scene.environment = texture;
+      const rgbeLoader = new RGBELoader();
+      // small_hangar_01_4k, rooftop_night_4k
+      rgbeLoader.load('hdri/small_hangar_01_4k.hdr', (texture) => {
+        texture.mapping = THREE.EquirectangularReflectionMapping;
+        this.scene.background = texture;
+        this.scene.environment = texture;
+        resolve();
+      }, undefined, (error) => {
+        console.error('Error loading HDRI:', error);
+        reject(error);
+      });
+
+      const planeGeometry = new THREE.PlaneGeometry(100, 100);
+      const planeMaterial = new THREE.MeshStandardMaterial({
+        color: 0x808080,
+        transparent: true,
+        opacity: 0.6
+      });
+      const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+      plane.rotation.x = - Math.PI / 2;
+      this.scene.add(plane);
+
+      const ambientLight = new THREE.AmbientLight(0x404040, 1.2);
+      this.scene.add(ambientLight);
+
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+      directionalLight.position.set(10, 10, 10);
+      directionalLight.castShadow = true;
+      this.scene.add(directionalLight);
+
+      this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+      this.controls.enableDamping = true;
+      this.controls.dampingFactor = 0.05;
+      this.controls.enableZoom = true;
+
+      window.addEventListener('resize', this.onWindowResize.bind(this));
     });
-
-    const planeGeometry = new THREE.PlaneGeometry(100, 100);
-    const planeMaterial = new THREE.MeshStandardMaterial({
-      color: 0x808080,
-      transparent: true,
-      opacity: 0.6
-    });
-    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.rotation.x = - Math.PI / 2;
-    this.scene.add(plane);
-
-    const ambientLight = new THREE.AmbientLight(0x404040, 1.2);
-    this.scene.add(ambientLight);
-
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(10, 10, 10);
-    directionalLight.castShadow = true;
-    this.scene.add(directionalLight);
-
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    this.controls.enableDamping = true;
-    this.controls.dampingFactor = 0.05;
-    this.controls.enableZoom = true;
-
-    window.addEventListener('resize', this.onWindowResize.bind(this));
   }
 
-  private loadModel(): void {
-    const loader = new GLTFLoader();
-    loader.load('models/cars/vw_polo_final.glb', (gltf: GLTF) => {
-      this.model = gltf.scene;
-      this.model.scale.set(1, 1, 1);
-      this.model.position.set(0, 0, 0);
+  private loadModel(): Promise<void> {
+    return new Promise((resolve, reject) => {
 
-      const box = new THREE.Box3().setFromObject(this.model);
-      const size = box.getSize(new THREE.Vector3());
-      const scaleFactor = 1 / Math.max(size.x, size.y, size.z);
-      this.model.scale.multiplyScalar(scaleFactor * 8);
+      const loader = new GLTFLoader();
+      loader.load('models/cars/vw_polo_final.glb', (gltf: GLTF) => {
+        this.model = gltf.scene;
+        this.model.scale.set(1, 1, 1);
+        this.model.position.set(0, 0, 0);
 
-      this.frontLeftDoor = this.model.getObjectByName('polo17_door_FL')!;
-      this.frontRightDoor = this.model.getObjectByName('polo17_door_FR')!;
-      this.backLeftDoor = this.model.getObjectByName('polo17_door_RL')!;
-      this.backRightDoor = this.model.getObjectByName('polo17_door_RR')!;
+        const box = new THREE.Box3().setFromObject(this.model);
+        const size = box.getSize(new THREE.Vector3());
+        const scaleFactor = 1 / Math.max(size.x, size.y, size.z);
+        this.model.scale.multiplyScalar(scaleFactor * 8);
 
-      this.carTrunk = this.model.getObjectByName('polo17_trunk')!;
-      this.carHood = this.model.getObjectByName('polo17_hood')!;
+        this.frontLeftDoor = this.model.getObjectByName('polo17_door_FL')!;
+        this.frontRightDoor = this.model.getObjectByName('polo17_door_FR')!;
+        this.backLeftDoor = this.model.getObjectByName('polo17_door_RL')!;
+        this.backRightDoor = this.model.getObjectByName('polo17_door_RR')!;
 
-      this.doorGroups = {
-        polo17_door_FL: [
-          this.model.getObjectByName('polo17_doorglass_FL001')!,
-          this.model.getObjectByName('polo17_doorpanel_FL')!,
-          this.model.getObjectByName('polo17_mirror_L')!
-        ],
-        polo17_door_FR: [
-          this.model.getObjectByName('polo17_doorglass_FL')!,
-          this.model.getObjectByName('polo17_doorpanel_FR')!,
-          this.model.getObjectByName('polo17_mirror_R')!
-        ],
-        polo17_door_RL: [
-          this.model.getObjectByName('polo17_doorglass_RR005')!,
-          this.model.getObjectByName('polo17_doorpanel_RL')!
-        ],
-        polo17_door_RR: [
-          this.model.getObjectByName('polo17_doorglass_RR004')!,
-          this.model.getObjectByName('polo17_doorpanel_RR')!
-        ]
-      }
+        this.carTrunk = this.model.getObjectByName('polo17_trunk')!;
+        this.carHood = this.model.getObjectByName('polo17_hood')!;
 
-      this.scene.add(this.model);
-    }, undefined, (error) => {
-      console.error('Error loading model:', error);
+        this.doorGroups = {
+          polo17_door_FL: [
+            this.model.getObjectByName('polo17_doorglass_FL001')!,
+            this.model.getObjectByName('polo17_doorpanel_FL')!,
+            this.model.getObjectByName('polo17_mirror_L')!
+          ],
+          polo17_door_FR: [
+            this.model.getObjectByName('polo17_doorglass_FL')!,
+            this.model.getObjectByName('polo17_doorpanel_FR')!,
+            this.model.getObjectByName('polo17_mirror_R')!
+          ],
+          polo17_door_RL: [
+            this.model.getObjectByName('polo17_doorglass_RR005')!,
+            this.model.getObjectByName('polo17_doorpanel_RL')!
+          ],
+          polo17_door_RR: [
+            this.model.getObjectByName('polo17_doorglass_RR004')!,
+            this.model.getObjectByName('polo17_doorpanel_RR')!
+          ]
+        }
+
+        this.scene.add(this.model);
+        this.modelsLoaderService.hide();
+        resolve();
+      }, undefined, (error) => {
+        this.modelsLoaderService.hide();
+        console.error('Error loading model:', error);
+        reject(error);
+      });
     });
   }
 
