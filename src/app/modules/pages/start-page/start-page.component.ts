bfb4e5588e11a9b3fa5514821fb411bda95e9f1f
AlexGervas <alexandragervas@gmail.com>
2025-06-10 18:40:15 +0700

    Updated the styles for locked levels

--- src/app/modules/pages/start-page/start-page.component.ts
+++ src/app/modules/pages/start-page/start-page.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { LevelService } from '../../../core/services/level.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-start-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './start-page.component.html',
   styleUrl: './start-page.component.css'
 })
 export class StartPageComponent {
-  constructor(private router: Router, private levelService: LevelService) { }
+  constructor(private router: Router, public levelService: LevelService) { }
 
   public startGame(level: string) {
     if(this.levelService.isLevelAvailable(level)) {
