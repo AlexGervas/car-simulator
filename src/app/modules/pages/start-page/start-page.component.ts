6e2c28200c3a910d5b6fcea50bcabb7080308a7a
AlexGervas <alexandragervas@gmail.com>
2025-06-11 21:22:05 +0700

    Fixed styles for locked levels

--- src/app/modules/pages/start-page/start-page.component.ts
+++ src/app/modules/pages/start-page/start-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LevelService } from '../../../core/services/level.service';
 import { CommonModule } from '@angular/common';
@@ -10,11 +10,23 @@ import { CommonModule } from '@angular/common';
   templateUrl: './start-page.component.html',
   styleUrl: './start-page.component.css'
 })
-export class StartPageComponent {
+export class StartPageComponent implements OnInit {
   constructor(private router: Router, public levelService: LevelService) { }
 
+  ngOnInit() {
+    const buttons = document.querySelectorAll('.map-button');
+    buttons.forEach((button: Element) => {
+      const level = button.getAttribute('name');
+      if (level && !this.levelService.isLevelAvailable(level)) {
+        button.classList.add('locked');
+      } else {
+        button.classList.remove('locked');
+      }
+    })
+  }
+
   public startGame(level: string) {
-    if(this.levelService.isLevelAvailable(level)) {
+    if (this.levelService.isLevelAvailable(level)) {
       this.router.navigate(['/game/simulator'], { queryParams: { level } });
     } else {
       alert("Этот уровень недоступен!");
