a5670755b8ccf69d99a0495da3a8772c7382796c
AlexGervas <alexandragervas@gmail.com>
2025-08-27 22:25:16 +0700

    Added registration component and input validations

---
+++ src/app/modules/auth/pages/registration/error-messages.ts
@@ -0,0 +1,7 @@
+export const ErrorMessages: { [key: string]: string } = {
+    required: 'Поле обязательно для заполнения',
+    email: 'Некорректный адрес электронной почты',
+    minlength: 'Минимум 8 символов',
+    pattern: 'Пароль должен включать латинские буквы, цифры и символы',
+    mismatchpasswords: 'Пароли не совпадают'
+}
--- src/app/modules/auth/pages/registration/registration.component.ts
+++ src/app/modules/auth/pages/registration/registration.component.ts
@@ -1,12 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
+import { ErrorMessages } from './error-messages';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIcon, RouterModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+
+  public registrationForm!: FormGroup;
+  public minLengthPass: number = 8;
+  public hide: boolean = true;
+  public hideConfirm: boolean = true;
+  public errorMsg = ErrorMessages;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.registrationForm = this.formBuilder.group({
+      userFirstName: ['', [Validators.required]],
+      userLastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(this.minLengthPass),
+        Validators.pattern(/(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*(){}":>?<~!"№;%:?*()]).{6,}/)
+      ]],
+      confirmPassword: ['', Validators.required]
+    },
+      { validators: this.checkPasswords.bind(this) }
+    );
+  }
+
+  public onSubmit(): void {
+    if (this.registrationForm.valid) {
+      console.log("New web user", this.registrationForm.value);
+    }
+  }
+
+  public getErrorMessage(controlName: string): string | null {
+    const control = this.registrationForm.get(controlName);
+
+    if (control && control.errors) {
+      const firstErrorKey = Object.keys(control.errors)[0];
+      return this.errorMsg[firstErrorKey];
+    }
+
+    return null;
+  }
+
+  private checkPasswords(group: AbstractControl): ValidationErrors | null {
+    const password = group.get('password');
+    const confirmPassword = group.get('confirmPassword');
+
+    if (!password || !confirmPassword) return null;
+
+    if (password.value !== confirmPassword.value) {
+      password.setErrors({ ...(password.errors || {}), mismatchpasswords: true });
+      confirmPassword.setErrors({ ...(confirmPassword.errors || {}), mismatchpasswords: true });
+    } else {
+      if (password.hasError('mismatchpasswords')) {
+        const errors = { ...(password.errors || {}) };
+        delete errors['mismatchpasswords'];
+        password.setErrors(Object.keys(errors).length ? errors : null);
+      }
+      if (confirmPassword.hasError('mismatchpasswords')) {
+        const errors = { ...(confirmPassword.errors || {}) };
+        delete errors['mismatchpasswords'];
+        confirmPassword.setErrors(Object.keys(errors).length ? errors : null);
+      }
+    }
+    return null;
+  }
 
 }
